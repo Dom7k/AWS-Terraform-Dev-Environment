5e9637b0d350a0c4fbfc24b40e81e9799428d5b1
Dom7k <dominicnaatey@gmail.com>
2024-05-11 08:23:24 +0000

    add AWS subnet resource

--- main.tf
+++ main.tf
@@ -7,3 +7,14 @@ resource "aws_vpc" "dom7_vpc" {
     Name = "dev"
   }
 }
+
+resource "aws_subnet" "dom7_public_subnet" {
+  vpc_id     = aws_vpc.dom7_vpc.id
+  cidr_block = "10.123.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "dev-public"
+  }
+}
