ece3774a866876fa990c7133a4f802412706c68c
Dom7k <dominicnaatey@gmail.com>
2024-05-11 17:08:37 +0000

    add AWS Route resource

--- main.tf
+++ main.tf
@@ -34,3 +34,9 @@ resource "aws_route_table" "dom7_public_rt" {
     Name = "dev-public-rt"
   }
 }
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.dom7_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.dom7_internet_gateway.id
+}
