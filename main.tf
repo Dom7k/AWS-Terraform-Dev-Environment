9236405a34f30c55be1b436521a8747c182eef52
Dom7k <dominicnaatey@gmail.com>
2024-05-11 16:53:22 +0000

    add AWS Internet Gateway resource

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "dom7_vpc" {
-  cidr_block = "10.123.0.0/16"
+  cidr_block           = "10.123.0.0/16"
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
 
   tags = {
     Name = "dev"
@@ -9,12 +9,20 @@ resource "aws_vpc" "dom7_vpc" {
 }
 
 resource "aws_subnet" "dom7_public_subnet" {
-  vpc_id     = aws_vpc.dom7_vpc.id
-  cidr_block = "10.123.1.0/24"
+  vpc_id                  = aws_vpc.dom7_vpc.id
+  cidr_block              = "10.123.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone = "us-west-2a"
+  availability_zone       = "us-west-2a"
 
   tags = {
     Name = "dev-public"
+  }
+}
+
+resource "aws_internet_gateway" "dom7_internet_gateway" {
+  vpc_id = aws_vpc.dom7_vpc.id
+
+  tags = {
+    Name = "dev-igw"
   }
 }
