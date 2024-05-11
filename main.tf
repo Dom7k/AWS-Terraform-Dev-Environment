d792ffe68afe81f69ee07c96ac2c005451e509c4
Dom7k <dominicnaatey@gmail.com>
2024-05-11 17:16:37 +0000

    add AWS Route Table Association resource

--- main.tf
+++ main.tf
@@ -40,3 +40,8 @@ resource "aws_route" "default_route" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.dom7_internet_gateway.id
 }
+
+resource "aws_route_table_association" "dom7_public_assoc" {
+  subnet_id      = aws_subnet.dom7_public_subnet.id
+  route_table_id = aws_route_table.dom7_public_rt.id
+}
