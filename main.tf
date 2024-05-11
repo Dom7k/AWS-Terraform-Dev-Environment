1fccc840ec0b606ddab716dcdca15e244c0f4f8a
Dom7k <dominicnaatey@gmail.com>
2024-05-11 18:35:57 +0000

    add AWS Security Group resource

--- main.tf
+++ main.tf
@@ -45,3 +45,23 @@ resource "aws_route_table_association" "dom7_public_assoc" {
   subnet_id      = aws_subnet.dom7_public_subnet.id
   route_table_id = aws_route_table.dom7_public_rt.id
 }
+
+resource "aws_security_group" "dom7_sg" {
+  name        = "dev_sg"
+  description = "dev security group"
+  vpc_id      = aws_vpc.dom7_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
